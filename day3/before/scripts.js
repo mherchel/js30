de8f10706400b97f211d43af701ef7d8705a7d0b
Mike Herchel <mike@herchel.com>
2016-12-31 09:32:45 -0500

    Color is working.

--- day3/before/scripts.js
+++ day3/before/scripts.js
@@ -1,15 +1,12 @@
 (function() {
   var rangeSpacingElement = document.querySelector('#range-spacing'),
       rangeBlurElement = document.querySelector('#range-blur'),
-      rangeColorElement = document.querySelector('#range-color');
-
-  var rangeSpacingVal = rangeSpacingElement.value,
-      rangeBlurVal = rangeBlurElement.value,
-      rangeColorVal = rangeColorElement.value;
+      inputColorElement = document.querySelector('#input-color');
 
   var rootElementStyles = getComputedStyle(document.documentElement),
       blurRadiusVal = String(rootElementStyles.getPropertyValue('--blur-radius')).trim(),
       spacingRadiusVal = String(rootElementStyles.getPropertyValue('--spacing')).trim();
+      inputColorVal = String(rootElementStyles.getPropertyValue('--color')).trim();
 
       rangeSpacingElement.addEventListener('input', function(e) {
         currentVal = this.value;
@@ -23,12 +20,22 @@
         console.log(currentVal);
       });
 
+      inputColorElement.addEventListener('input', function(e) {
+        currentVal = this.value;
+        setColor(currentVal);
+        console.log(currentVal);
+      });
+
   function setSpacing(val) {
     document.documentElement.style.setProperty('--spacing', val + 'px');
   }
 
   function setBlur(val) {
     document.documentElement.style.setProperty('--blur-radius', val + 'px');
+  }
+
+  function setColor(val) {
+    document.documentElement.style.setProperty('--color', val);
   }
 
 })();
