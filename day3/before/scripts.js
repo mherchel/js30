faffcd505ae8d618d5314208304fad4c705ddb62
Mike Herchel <mike@herchel.com>
2016-12-30 19:28:53 -0500

    Blur and spacing working.

--- day3/before/scripts.js
+++ day3/before/scripts.js
@@ -1,3 +1,34 @@
 (function() {
+  var rangeSpacingElement = document.querySelector('#range-spacing'),
+      rangeBlurElement = document.querySelector('#range-blur'),
+      rangeColorElement = document.querySelector('#range-color');
+
+  var rangeSpacingVal = rangeSpacingElement.value,
+      rangeBlurVal = rangeBlurElement.value,
+      rangeColorVal = rangeColorElement.value;
+
+  var rootElementStyles = getComputedStyle(document.documentElement),
+      blurRadiusVal = String(rootElementStyles.getPropertyValue('--blur-radius')).trim(),
+      spacingRadiusVal = String(rootElementStyles.getPropertyValue('--spacing')).trim();
+
+      rangeSpacingElement.addEventListener('input', function(e) {
+        currentVal = this.value;
+        setSpacing(currentVal);
+        console.log(currentVal);
+      });
+
+      rangeBlurElement.addEventListener('input', function(e) {
+        currentVal = this.value;
+        setBlur(currentVal);
+        console.log(currentVal);
+      });
+
+  function setSpacing(val) {
+    document.documentElement.style.setProperty('--spacing', val + 'px');
+  }
+
+  function setBlur(val) {
+    document.documentElement.style.setProperty('--blur-radius', val + 'px');
+  }
 
 })();
